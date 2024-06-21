4979ffcd56d6ffb302e10a680beb9a817e3963cf
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-06-21 14:26:29 +0200

    landingspage af

---
+++ template-parts/footer.php
@@ -0,0 +1,63 @@
+ <!-- contact-section -->
+ <section class="contact">
+        <div class="contact-info">
+            <div class="first-info">
+                <img src="image/logo.png" alt="">
+
+                <p>3245 Grant Street Longview, <br> TX Noord-Holland 765378</p>
+                <p>phone number: 01601348732</p>
+                <p>WebshopKumo3080@gmail.com</p>
+
+                <div class="social-icon">
+                    <a href=""><i class='bx bxl-facebook'></i></a>
+                    <a href=""><i class='bx bxl-twitter'></i></a>
+                    <a href=""><i class='bx bxl-instagram'></i></a>
+                    <a href=""><i class='bx bxl-youtube'></i></a>
+                    <a href=""><i class='bx bxl-linkedin'></i></a>
+                </div>
+            </div>
+
+            <div class="second-info">
+                <h4>Support</h4>
+                <p>Contact Us</p>
+                <p>About Page</p>
+                <p>Size Guide</p>
+                <p>Shopping & Resturns</p>
+                <p>Privacy</p>
+            </div>
+
+            <div class="third-info">
+                <h4>Shop</h4>
+                <p>Men's Shopping</p>
+                <p>Women's Shopping</p>
+                <p>Kid's Shopping</p>
+                <p>Furniture</p>
+                <p>Discount</p>
+            </div>
+
+            <div class="fourth-info">
+                <h4>Company</h4>
+                <p>About</p>
+                <p>Blog</p>
+                <p>Affilate</p>
+                <p>Login</p>
+            </div>
+
+            <div class="five">
+                <h4>Subscribe</h4>
+                <p>Als je je abonneert op onze webshop, krijg je exclusieve voordelen zoals vroegtijdige toegang tot
+                    nieuwe collecties, speciale kortingen en persoonlijke aanbiedingen die alleen beschikbaar zijn voor
+                    abonnees. <br> Mis deze kans niet om als eerste te profiteren van onze beste deals en unieke producten!
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <div class="end-text">
+        <p>Copyright @2024. All Rights Reserved. Designd By Giorgina Cali</p>
+    </div>
+
+    <script src="/js/main.js" defer></script>
+</body>
+
+</html>
